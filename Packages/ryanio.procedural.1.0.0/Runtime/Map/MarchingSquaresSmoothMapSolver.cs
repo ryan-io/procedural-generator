e06fdc8af1f372721abce838c5518501cf4f7b5e
Ryan Developer - Unity <47833404+ryanunitydeveloper@users.noreply.github.com>
2023-09-07 06:24:06 -0500

    Undocumented changes

--- Packages/ryanio.procedural.1.0.0/Runtime/Map/MarchingSquaresSmoothMapSolver.cs
+++ Packages/ryanio.procedural.1.0.0/Runtime/Map/MarchingSquaresSmoothMapSolver.cs
@@ -25,18 +25,15 @@ namespace ProceduralGeneration {
 			using (m_GetSmoothedMap.Auto()) {
 				var rows    = original.Height;
 				var columns = original.Width;
-
+				
 				// for (int i = 0; i < rows * columns; i++) {
 				// 	int x = i % columns, y = i / rows;
-				// 	copy = DetermineNeighborLimits(ref x, ref y, copy, original);
+				// 	copy = DetermineNeighborLimits( x,  y, copy, original);
 				// }
 
 				for (var x = 0; x < rows; x++) {
 					for (var y = 0; y < columns; y++) {
-						//var tempCopy = 
-							DetermineNeighborLimits(ref x, ref y, original, copy);
-						// copy.Clear();
-						// tempCopy.CopyTo(copy);
+						DetermineNeighborLimits(x, y, original, copy);
 					}
 				}
 
@@ -48,9 +45,9 @@ namespace ProceduralGeneration {
 
 		static readonly ProfilerMarker m_DetermineNeighborLimits = new(nameof(DetermineNeighborLimits));
 
-		void DetermineNeighborLimits(ref int x, ref int y, Span2D<int> original, Span2D<int> copy) {
+		void DetermineNeighborLimits(int x, int y, Span2D<int> original, Span2D<int> copy) {
 			using (m_DetermineNeighborLimits.Auto()) {
-				var surroundingWalls = GetAdjacentWallsCount(ref x, ref y, original);
+				var surroundingWalls = GetAdjacentWallsCount(x, y, original);
 
 				if (surroundingWalls > COMPARISON_LIMIT)
 					copy[x, y] = 1;
@@ -64,13 +61,13 @@ namespace ProceduralGeneration {
 
 		static readonly ProfilerMarker m_GetAdjacentWallsCount = new(nameof(GetAdjacentWallsCount));
 
-		int GetAdjacentWallsCount(ref int x, ref int y, Span2D<int> original) {
+		int GetAdjacentWallsCount(int x, int y, Span2D<int> original) {
 			using (m_GetAdjacentWallsCount.Auto()) {
 				var count = 0;
 
 				for (var neighborX = x - 1; neighborX <= x + 1; neighborX++) {
 					for (var neighborY = y - 1; neighborY <= y + 1; neighborY++)
-						count = DetermineCount(ref x, ref y, ref neighborX, ref neighborY, ref count, original);
+						count = DetermineCount(x, y, neighborX, neighborY, count, original);
 				}
 
 				return count;
@@ -79,10 +76,10 @@ namespace ProceduralGeneration {
 
 		static readonly ProfilerMarker m_DetermineCount = new(nameof(DetermineCount));
 
-		int DetermineCount(ref int gridX, ref int gridY, ref int neighborX, ref int neighborY, ref int count,
+		int DetermineCount(int gridX, int gridY, int neighborX, int neighborY, int count,
 			Span2D<int> original) {
 			using (m_DetermineCount.Auto()) {
-				if (IsWithinBoundary(ref neighborX, ref neighborY)) {
+				if (IsWithinBoundary(neighborX, neighborY)) {
 					if (neighborX != gridX || neighborY != gridY)
 						count += original[neighborX, neighborY];
 				}
@@ -97,7 +94,7 @@ namespace ProceduralGeneration {
 
 		static readonly ProfilerMarker m_IsWithinBoundary = new(nameof(IsWithinBoundary));
 
-		bool IsWithinBoundary(ref int neighborX, ref int neighborY) {
+		bool IsWithinBoundary(int neighborX, int neighborY) {
 			using (m_IsWithinBoundary.Auto()) {
 				return neighborX > BORDER_SAFETY_FACTOR                       &&
 				       neighborX < Ctx.Dimensions.Rows - BORDER_SAFETY_FACTOR &&
